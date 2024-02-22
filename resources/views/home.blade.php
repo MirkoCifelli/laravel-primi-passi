19efdaa8fdc7c09ad19f018d24f55bc6620051cd
MirkoCifelli <ciffo4@hotmail.it>
2024-02-22 13:55:10 +0100

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel</title>
+</head>
+<body>
+    <header>
+        <h1>
+            Prova laravel
+        </h1>
+    </header>
+
+
+    <main>
+        <h3>
+            Hello World
+        </h3>
+    </main>
+</body>
+</html>
